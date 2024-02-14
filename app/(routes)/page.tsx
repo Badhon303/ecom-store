c0dd98a3a245db18cdefb429f9a0177c9cfef3cb
BadhonDev <badhon@ebiw.com>
2024-02-14 15:56:36 +0600

    id changed

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ import Container from "@/components/ui/container"
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true })
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b")
+  const billboard = await getBillboard("ddc52fb6-b15c-4101-9aa4-151af6939f86")
 
   return (
     <Container>
